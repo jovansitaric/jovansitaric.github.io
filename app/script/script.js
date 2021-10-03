f9943b5d9c4adc139784ecb296609bddf6d66dfb
jovansitaric <jsitaric@gmail.com>
2021-10-03 11:23:47 -0700

    Many minor improvements

--- app/script/script.js
+++ app/script/script.js
@@ -14,7 +14,7 @@
 
                 heroText.forEach((item, index) => {
                     item.classList.add('-show', '-offset');
-                    
+
                     // Add rest of the name to first heading
                     if(index == 0) {
                         setTimeout( () => {
@@ -27,7 +27,80 @@
                     item.classList.add('-animate');
                 });
             }
-        }
+        };
+
+        const navbar = {
+
+            init: () => {
+                navbar.add();
+            },
+
+            add: () => {
+                const navbar = document.querySelector('.js-navbar');
+
+                if(window.pageYOffset > 0) {
+                    navbar.classList.add('-sticky');
+                }
+
+                else {
+                    navbar.classList.remove('-sticky');
+                }
+            }
+        };
+
+        const sections = {
+
+            init: () => {
+                sections.count();
+            },
+
+            count: () => {
+                const getAllSections = document.querySelectorAll('.js-sections');
+
+                getAllSections.forEach((item, index) => {
+
+                    if(index == 0) {
+                        item.classList.add('-first');
+                    }
+
+                    else if(index % 2 == 0) {
+                        item.classList.add('-even');
+                    }
+
+                    else {
+                        item.classList.add('-odd');
+                    }
+                });
+            }
+        };
+
+        const disableLinks = {
+
+            init: () => {
+                disableLinks.disable();
+            },
+
+            disable: () => {
+                // Get all empty links
+                const getAllLinks = document.querySelectorAll('a');
+
+                getAllLinks.forEach(item => {
+                    
+                    if (item.attributes.href.value === '#') {
+                        
+                        item.addEventListener('click', (event) => {
+                            event.preventDefault();
+                        });
+                    }
+                });
+            }
+        };
+
+        const parallax = {};
+
+        /**
+         * Initializing functions
+         */
 
         AOS.init({
             duration: 800,
@@ -35,5 +108,11 @@
         });
 
         add.init();
+        sections.init();
+        disableLinks.init();
+
+        window.addEventListener('scroll', () => {
+            navbar.init();
+        })
     });
 }());
